ed1275d13df5b82f4e74bd56abfe5bb64e682dd6
v.sherina <v.sherina@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-03-09 22:26:02 +0000

    Bugs fixed, Batch effect addition
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/nondetects@114561 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/qpcrImpute.R
+++ R/qpcrImpute.R
@@ -1,5 +1,5 @@
 # actual function
-qpcrImpute <- function(object, dj=NULL, pyfit=NULL, groupVars=NULL, tol=1,
+qpcrImpute <- function(object, dj=NULL, pyfit=NULL, groupVars=NULL, batch=NULL, tol=1,
                        iterMax=100, outform=c("Single","Param","Multy"), formula=NULL, numsam=5, 
                        vary_fit=TRUE, vary_model=TRUE, add_noise=TRUE)
 {
@@ -39,48 +39,77 @@ qpcrImpute <- function(object, dj=NULL, pyfit=NULL, groupVars=NULL, tol=1,
       }
     }
   }
-
+  ## accounting for batch effect 
+  if (!is.null(batch)) 
+     {
+      B <- batch
+     } else { 
+      B <- matrix(0, nrow=nrow(exprs(object)), ncol=ncol(exprs(object)))
+  }
+  
+  
   ## select target genes
   ind <- grep("target", featureType(object), ignore.case=TRUE)
   if(length(ind) == 0) stop("No target genes found in featureType(object).")
 
   ## sample grouping variable(s)
-  if(is.null(groupVars)){
-    nrep <- apply(pData(object), 1, paste, collapse=":")
-  } else {
-    if(length(groupVars)>1){
-      nrep <- apply(pData(object)[,groupVars], 1, paste, collapse=":")
-    } else nrep <- pData(object)[,groupVars]
+  if(is.null(formula)){
+    if(is.null(groupVars)){
+      nrep <- apply(pData(object), 1, paste, collapse=":")
+    } else {
+      if(length(groupVars)>1){
+        nrep <- apply(pData(object)[,groupVars], 1, paste, collapse=":")
+      } else nrep <- pData(object)[,groupVars]
+    }
+    if(class(nrep)!="character"){
+    nrep <- droplevels(nrep)
+    }
+    if(length(levels(nrep))==1) formula=as.formula("~1") else formula=as.formula("~0+nrep")
+    DesLM=model.matrix(formula,as.data.frame(nrep))
+  } else{
+    formula <- as.formula(formula)
+    DesLM=model.matrix(formula, data=pData(object))
   }
-  formula=as.formula("~0+nrep")
   print(formula)
-
-  Y <- sweep(exprs(object)[ind,], 2, dj)
-  DesLM=model.matrix(formula,as.data.frame(nrep))
-  tst=lmFit(Y, design=DesLM)
+  
+  ## take out batch effect
+  Ct.nobatch <- exprs(object)-B
+  Y          <- sweep(Ct.nobatch[ind,], 2, dj)
+  tst        =  lmFit(Y, design=DesLM)
 
   repName <- nrep
   nrep <- as.numeric(as.factor(nrep))
 
   ## initial fit
   if(is.null(pyfit)){
-    p.nd <- as.vector(
-      apply(featureCategory(object)[ind,], 1,
-            function(x) by(x == "Undetermined", nrep, mean)))
-    gavg <- as.vector(
-      apply(exprs(object)[ind,], 1, function(x) by(x, nrep, median)))
-    ws <- as.vector(
-      apply(featureCategory(object)[ind,], 1,
-            function(x) by(x, nrep, length)))
+    if(length(nrep)>1){
+    
+      p.nd <- as.vector(
+        apply(featureCategory(object)[ind,], 1,
+              function(x) by(x == "Undetermined", nrep, mean)))
+      gavg <- as.vector(
+        apply(exprs(object)[ind,], 1, function(x) by(x, nrep, median)))
+      ws <- as.vector(
+        apply(featureCategory(object)[ind,], 1,
+              function(x) by(x, nrep, length)))
+    } else {
+        p.nd <- as.vector(
+          apply(featureCategory(object)[ind,], 1,
+                function(x) mean(x == "Undetermined")))
+        gavg <- as.vector(
+          apply(exprs(object)[ind,], 1, median))
+        ws <- as.vector(
+          apply(featureCategory(object)[ind,], 1, length))
+      }
     pyfit <- glm(p.nd~gavg, family=binomial(link=logit), weights=ws)
   }
 
-
   ## format data for EM algorithm
-  Ct <- as.vector(exprs(object)[ind,])
+  Ct    <- as.vector(exprs(object)[ind,])
+  B.vec <- as.vector(B[ind,])
   ngene <- as.numeric(as.factor(rep((1:nrow(object))[ind], ncol(object))))
-  i.nd <- as.vector(featureCategory(object)[ind,] == "Undetermined")
-  dj <- rep(dj, each=length(ind))
+  i.nd  <- as.vector(featureCategory(object)[ind,] == "Undetermined")
+  dj    <- rep(dj, each=length(ind))
   ntype <- rep(nrep, each=length(ind))
 
   ## initial means
@@ -89,16 +118,15 @@ qpcrImpute <- function(object, dj=NULL, pyfit=NULL, groupVars=NULL, tol=1,
 
   thetaVec.tst <- vector(length=length(Ct))
   thetaMat.tst <- matrix(nrow=max(ngene), ncol=max(ntype))
-
+  #####
   for(i in 1:max(ngene)){
     for(j in 1:max(ntype)){
       ind2 <- intersect(which(ngene == i), which(ntype == j))
-      thetaVec[ind2] <- thetaMat[i,j] <- mean(Ct[ind2]-dj[ind2])
-      #thetaVec[ind2] <- thetaMat[i,j] <- mean(Ct[ind2])
+      ###
+      thetaVec[ind2] <- thetaMat[i,j] <- mean(Ct[ind2]-dj[ind2]-B.vec[ind2])
       thetaVec.tst[ind2]<-thetaMat.tst[i,j]<-tst$coefficients[i,j]
     }
   }
-  #    cat("initial mean:",head(thetaMat),"\n")
 
   ## initial variance
   s2Vec <- vector(length=length(Ct))
@@ -106,26 +134,27 @@ qpcrImpute <- function(object, dj=NULL, pyfit=NULL, groupVars=NULL, tol=1,
 
   s2Vec.tst <- vector(length=length(Ct))
   Name.s2<-s2Mat.tst <- matrix(nrow=max(ngene), ncol=max(ntype))
-
+  #####
   for(i in 1:max(ngene)){
     for(j in 1:max(ntype)){
-      ind2 <- which(ngene == i)
-      ##s2Vec[ind2] <- s2Mat[i,j] <- var(Ct[ind2]-thetaVec[ind2]-dj[ind2])
-      s2Vec[ind2] <- s2Mat[i,j] <- sum((Ct[ind2]-thetaVec[ind2]-dj[ind2])^2)/(length(ind2)-max(ntype))
+      ind2            <- which(ngene == i)
+  ###
+      s2Vec[ind2]     <- s2Mat[i,j] <- sum((Ct[ind2]-thetaVec[ind2]-dj[ind2]-B.vec[ind2])^2)/(length(ind2)-max(ntype))
       s2Vec.tst[ind2] <- s2Mat.tst[i,j] <- tst$sigma[i]^2
-      Name.s2[ind2] <- names(tst$sigma[i])
+      Name.s2[ind2]   <- names(tst$sigma[i])
     }
   }
   names(s2Vec.tst) <- Name.s2
+  
   ## begin EM algorithm
   params.new <- params.old <- list(
-    s2Vec=s2Vec.tst,
-    s2Mat=s2Mat.tst,
-    thetaVec=thetaVec.tst,
-    thetaMat=thetaMat.tst,
-    sigma=tst$sigma,
-    cov.matrix=tst$cov.coefficients)
-
+                                    s2Vec=s2Vec.tst,
+                                    s2Mat=s2Mat.tst,
+                                    thetaVec=thetaVec.tst,
+                                    thetaMat=thetaMat.tst,
+                                    sigma=tst$sigma,
+                                    cov.matrix=tst$cov.coefficients
+                                  )
   ll <- vector(length=iterMax)
   iter <- 1
   cond <- TRUE
@@ -141,14 +170,14 @@ qpcrImpute <- function(object, dj=NULL, pyfit=NULL, groupVars=NULL, tol=1,
     ez <- rep(NA, length=length(Ct))
     for(i in 1:length(Ct)){
       if(i.nd[i]){
-        xi <- params.new$thetaVec[i]+dj[i]
+        #####
+        xi <- params.new$thetaVec[i]+dj[i]+B.vec[i]
         ez[i] <- EZ(xi, params.new$s2Vec[i], pyfit)
       }
     }
 
     ## M-Step -- theta
-    thetas <- updateTheta(Y, ez, dj, i.nd, ngene, ntype, DesLM)
-    # thetas <- updateTheta(Ct, ez, dj, i.nd, ngene, ntype)
+    thetas <- updateTheta(Y, ez, dj, B.vec, i.nd, ngene, ntype, DesLM)
     params.new$thetaVec   <- thetas[[1]]
     params.new$thetaMat   <- thetas[[2]]
     params.new$sigma      <- thetas[[3]]
@@ -159,21 +188,22 @@ qpcrImpute <- function(object, dj=NULL, pyfit=NULL, groupVars=NULL, tol=1,
     ez <- ez2 <- rep(NA, length=length(Ct))
     for(i in 1:length(Ct)){
       if(i.nd[i]){
-        xi <- params.new$thetaVec[i]+dj[i]
+        #####
+        xi <- params.new$thetaVec[i]+dj[i]+B.vec[i]
         ez[i] <- EZ(xi, params.new$s2Vec[i], pyfit)
         ez2[i] <- EZ2(xi, params.new$s2Vec[i], pyfit, ez[i])
       }
     }
 
     ## M-Step -- sigma2
-    sigmas <- updateS2(Ct, params.new$thetaVec, dj, ez, ez2,
+    sigmas <- updateS2(Ct, params.new$thetaVec, dj, B.vec, ez, ez2,
                        i.nd, ngene, ntype)
     params.new$s2Vec <- sigmas[[1]]
     params.new$s2Mat <- sigmas[[2]]
 
-
+    #####
     ll[iter] <- logLik(Ct, ez, ez2, params.new$s2Vec,
-                       params.new$thetaVec, dj, i.nd)
+                       params.new$thetaVec, dj, B.vec, i.nd)
     message(ll[iter])
 
     if(iter>1) cond <- (abs(ll[iter]-ll[iter-1]) > tol) & (iter < iterMax)
@@ -190,12 +220,12 @@ qpcrImpute <- function(object, dj=NULL, pyfit=NULL, groupVars=NULL, tol=1,
 
   ############# if statement about returning parameters or values
   print(outform)
-
+  #####
   if (outform == "Multy") {
-    multylist<-multy(object, pyfit, numsam, params.new, Ct, Y, dj, ez, ez2,
+    multylist<-multy(object, pyfit, numsam, params.new, Ct, Y, dj, B.vec, ez, ez2,
                      i.nd, ngene, ntype, DesLM, iterMax, tol,
                      vary_fit, vary_model, add_noise)
-  }
+  } 
   else {
     if (outform == "Single") {
       Ct[which(as.logical(i.nd))] <- ez[which(as.logical(i.nd))]
@@ -219,11 +249,11 @@ qpcrImpute <- function(object, dj=NULL, pyfit=NULL, groupVars=NULL, tol=1,
     else {
       if (outform == "Param") {
         sigma2<-params.new$s2Mat.tst
-        # colnames(sigma2) <- unique(repName)
         Parameters <- list("sigma2"=params.new$s2Mat,
                            "theta"=params.new$thetaMat,
                            "formula"=formula, "tst"=tst)
-        colnames(Parameters$sigma2) <- colnames(Parameters$theta) <- unique(repName)
+        colnames(Parameters$sigma2) <- colnames(Parameters$theta) <- substring(colnames(Parameters$tst$design),5)
+        rownames(Parameters$sigma2) <- rownames(Parameters$theta) <- names(Parameters$tst$Amean)
         message("Parameters are saved in the list")
         return(Parameters)}
     }
